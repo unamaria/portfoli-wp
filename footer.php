9c850bb2d7724b096cb4ca2d704203d3b119768b
Maria <unamaria@gmail.com>
2015-05-13 18:47:48 -0500

    Flex no wrap footer ul

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 		<footer>
 			<ul>
-				<li><a href="https://github.com/unamaria"  target="_blank"><img srcset="<?php bloginfo('template_url'); ?>/images/social/github.svg" src="<?php bloginfo('template_url'); ?>/images/social/github.png" alt="GitHub"></a></li>
-				<li><a href="https://twitter.com/una_maria" target="_blank"><img srcset="<?php bloginfo('template_url'); ?>/images/social/twitter.svg" src="<?php bloginfo('template_url'); ?>/images/social/twitter.png" alt="Twitter"></a></li>
-				<li><a href="https://www.flickr.com/photos/unamaria/" target="_blank"><img srcset="<?php bloginfo('template_url'); ?>/images/social/flickr.svg" src="<?php bloginfo('template_url'); ?>/images/social/flickr.png" alt="Flickr"></a></li>
-				<li><a href="https://www.linkedin.com/in/mariacomas" target="_blank"><img srcset="<?php bloginfo('template_url'); ?>/images/social/linkedin.svg" src="<?php bloginfo('template_url'); ?>/images/social/linkedin.png" alt="LinkedIn"></a></li>				
+				<li><a href="https://github.com/unamaria"><img srcset="<?php bloginfo('template_url'); ?>/images/social/github.svg" src="<?php bloginfo('template_url'); ?>/images/social/github.png" alt="GitHub"></a></li>
+				<li><a href="https://twitter.com/una_maria"><img srcset="<?php bloginfo('template_url'); ?>/images/social/twitter.svg" src="<?php bloginfo('template_url'); ?>/images/social/twitter.png" alt="Twitter"></a></li>
+				<li><a href="https://www.flickr.com/photos/unamaria/"><img srcset="<?php bloginfo('template_url'); ?>/images/social/flickr.svg" src="<?php bloginfo('template_url'); ?>/images/social/flickr.png" alt="Flickr"></a></li>
+				<li><a href="https://www.linkedin.com/in/mariacomas"><img srcset="<?php bloginfo('template_url'); ?>/images/social/linkedin.svg" src="<?php bloginfo('template_url'); ?>/images/social/linkedin.png" alt="LinkedIn"></a></li>				
 			</ul>
 			<a href="#header">↑ Back to top</a>
 			<small>&copy;<?php echo " "; echo date("Y"); echo " "; bloginfo('name'); ?></small>
