9763cf4b026787173d5fa021fb5e66e4a8fd32a8
Maria <unamaria@gmail.com>
2015-05-15 16:29:25 -0500

    Text instead of icons in social footer

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 		<footer>
-			<ul>
-				<li><a href="https://github.com/unamaria"><img srcset="<?php bloginfo('template_url'); ?>/images/social/github.svg" src="<?php bloginfo('template_url'); ?>/images/social/github.png" alt="GitHub"></a></li>
-				<li><a href="https://twitter.com/una_maria"><img srcset="<?php bloginfo('template_url'); ?>/images/social/twitter.svg" src="<?php bloginfo('template_url'); ?>/images/social/twitter.png" alt="Twitter"></a></li>
-				<li><a href="https://www.flickr.com/photos/unamaria/"><img srcset="<?php bloginfo('template_url'); ?>/images/social/flickr.svg" src="<?php bloginfo('template_url'); ?>/images/social/flickr.png" alt="Flickr"></a></li>
-				<li><a href="https://www.linkedin.com/in/mariacomas"><img srcset="<?php bloginfo('template_url'); ?>/images/social/linkedin.svg" src="<?php bloginfo('template_url'); ?>/images/social/linkedin.png" alt="LinkedIn"></a></li>				
+			<ul class="social">
+				<li><a href="https://github.com/unamaria">GitHub</a></li>
+				<li><a href="https://twitter.com/una_maria">Twitter</a></li>
+				<li><a href="https://www.flickr.com/photos/unamaria/">Flickr</a></li>
+				<li><a href="https://www.linkedin.com/in/mariacomas">LinkedIn</a></li>				
 			</ul>
 			<a href="#header">↑ Back to top</a>
 			<small>&copy;<?php echo " "; echo date("Y"); echo " "; bloginfo('name'); ?></small>
